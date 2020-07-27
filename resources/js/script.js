c6ce528f47fd888cf6b40a455648dffd78c2db54
ruscat <rus.reg.36@gmail.com>
2020-07-27 08:35:45 +0300

    054

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,22 @@
 $(document).ready(function () {
-  $('h1').click(function () {
-    $(this).css('background-color', '#ff0000');
-  });
+  $('.js--section-features').waypoint(
+    function (direction) {
+      if (direction === 'down') {
+        $('nav').addClass('sticky');
+      } else {
+        $('nav').removeClass('sticky');
+      }
+    },
+    {
+      offset: '60px',
+    }
+  );
+  // const waypoints = $('#handler-first').waypoint(
+  //   function (direction) {
+  //     notify(`${this.element.id} hit 25% from top of window`);
+  //   },
+  //   {
+  //     offset: '25%',
+  //   }
+  // );
 });
