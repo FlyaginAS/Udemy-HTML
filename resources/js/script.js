7f00dfcc66a49c4e4d49ac97541459e30a0c5b01
ruscat <rus.reg.36@gmail.com>
2020-07-27 12:08:26 +0300

    057

--- resources/js/script.js
+++ resources/js/script.js
@@ -106,3 +106,19 @@ $(document).ready(function () {
     }
   );
 });
+
+/*Mobile navigation*/
+$('.js--nav-icon').click(function () {
+  const nav = $('.js--main-nav');
+  const icon = $('.js--nav-icon i');
+
+  nav.slideToggle(200);
+
+  if (icon.hasClass('ion-navicon-round')) {
+    icon.addClass('ion-close-round');
+    icon.removeClass('ion-navicon-round');
+  } else {
+    icon.addClass('ion-navicon-round');
+    icon.removeClass('ion-close-round');
+  }
+});
